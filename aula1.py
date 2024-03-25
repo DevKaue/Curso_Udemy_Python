b6d06be3d0b63cef2383b9306462655a93c05e9a
Kaue Dev <kauesabino@hotmail.com>
2024-03-25 15:00:18 -0300

    Entendendo comentarios em python

---
+++ aula1.py
@@ -0,0 +1,15 @@
+""" 
+Comentário em área
+Também se pode utilizar a aspas simples exemplo:
+
+'''
+'''
+
+"""
+
+
+# Permite escrever um comentario 
+
+print(123) # Exibe algum conteudo no console
+#Abaixo
+print(456)
